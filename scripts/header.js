73f8d9967caf0b63b6ad0984403423ec76d6f346
FrankAlbella <frankalbella@gmail.com>
2019-09-09 16:38:24 -0400

    Add header-container eventlistener

---
+++ scripts/header.js
@@ -0,0 +1,11 @@
+function toggleHeader()
+{
+    document.getElementsByClassName('corner-caret')[0].classList.toggle('active');
+    document.getElementsByClassName('header-containter')[0].classList.toggle('active'):
+}
+
+
+document.addEventListener('DOMContentLoaded', function () 
+{
+	document.getElementsByClassName('corner-caret')[0].addEventListener('click', );
+});
