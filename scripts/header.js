4f3804f4ffb848bff59bc0cf9b5ff7bc6e34de38
FrankAlbella <frankalbella@gmail.com>
2019-09-09 16:42:26 -0400

    Fix typo in header.js

--- scripts/header.js
+++ scripts/header.js
@@ -1,11 +1,11 @@
 function toggleHeader()
 {
     document.getElementsByClassName('corner-caret')[0].classList.toggle('active');
-    document.getElementsByClassName('header-containter')[0].classList.toggle('active'):
+    document.getElementsByClassName('header-container')[0].classList.toggle('active');
 }
 
 
 document.addEventListener('DOMContentLoaded', function () 
 {
-	document.getElementsByClassName('corner-caret')[0].addEventListener('click', );
+	document.getElementsByClassName('corner-caret')[0].addEventListener('click', toggleHeader);
 });
